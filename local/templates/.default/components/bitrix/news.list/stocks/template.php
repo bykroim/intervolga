df716c8bae4696e6cbe4dbaa1de8687189aa4719
FaQBoss <bykroim2@mail.ru>
2022-02-12 00:10:14 +0300

    EDU_OR-93 - задание. Ссылка на новость с заголовком браузера - https://ishalin2.ivsupport.ru/news/?ELEMENT_ID=3

--- local/templates/.default/components/bitrix/news.list/stocks/template.php
+++ local/templates/.default/components/bitrix/news.list/stocks/template.php
@@ -1,4 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
+
 <?foreach($arResult["ITEMS"] as $arItem):?>
 <div class="sb_action">
     <a href=""><?if(is_array($arItem["PREVIEW_PICTURE"])):?>
@@ -8,7 +9,7 @@
     <h5><a href=""><?echo $arItem["NAME"]?></a></h5>
     <h5><?=GetMessage('DESCRIPTION')?> <?echo $arItem["PREVIEW_TEXT"];?></h5>
     <h5><?=GetMessage('PRICE')?> <?echo $arItem["PROPERTIES"]['price']['VALUE'];?> rub</h5>
-    <a href="<?echo $arItem["PROPERTIES"]['link']['VALUE'];?>" class="sb_action_more"><?=GetMessage('MORE_DETAILLED')?></a>
+    <a href="<?echo $arItem["DETAIL_PAGE_URL"];?>" class="sb_action_more"><?=GetMessage('MORE_DETAILLED')?></a>
 </div>
 <?endforeach;?>
 
